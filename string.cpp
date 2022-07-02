d9b8b7fb3104bfc01d1be3fba859458dc28d303d
tardis1 <bigmanrunning@gmail.com>
2022-07-02 16:31:46 -0500

    strings 07/02/2022

--- string.cpp
+++ string.cpp
@@ -14,4 +14,24 @@ int main()
 
     std::cout << s3 << std::endl;
 
+    std::cout << s.at(3) << std::endl;
+    std::cout << s2[3] << std::endl;
+
+    std::cout << "Give your response: ";
+    std::string response;
+    std::getline(std::cin, response);   //This is new. Getting string input
+    
+    //If you don't use getline/cin, the strings won't be equal
+    if(response != "I'm fine")
+    {
+        std::cout << "You didn't answer I'm fine. Is something wrong?" << std::endl;
+    }
+    else
+    {
+        std::cout << "Great! Glad to hear that." << std::endl;
+        std::cout << "You entered: " << response.c_str() << std::endl;
+        // .c_str() is a method that returns a pointer to first character of string
+        
+    }
+
 }
